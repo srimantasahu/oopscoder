8feee2cf028608a67417c1c65a24c4231491146b
srimantasahu <srimant.sahu@gmail.com>
2025-04-10 23:33:46 +0530

    added java concurrency topics till threadpoolexecutor

--- src/main/setup/functions.php
+++ src/main/setup/functions.php
@@ -2,6 +2,13 @@
 
 if ( ! function_exists( 'generate_setup' ) ) {
 
+    function generate_setup() {
+
+        // Added towards the end - to suppress SyntaxHighlighter Evolved
+        // turning double quotes (") into &quot; in the Java code area
+        remove_filter('the_content', 'wptexturize');
+    }
+
 	// Added towards the end - for overriding footer notes
 	add_filter('generate_copyright', 'custom_generatepress_footer');
 
