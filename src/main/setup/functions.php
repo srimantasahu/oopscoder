53257606cc62b805124b6c87a768c6f73c02772c
srimantasahu <srimant.sahu@gmail.com>
2025-04-16 16:37:13 +0530

    added oopscoder logo for login page

--- src/main/setup/functions.php
+++ src/main/setup/functions.php
@@ -9,6 +9,25 @@ if ( ! function_exists( 'generate_setup' ) ) {
         remove_filter('the_content', 'wptexturize');
     }
 
+    // Added for loging page logo
+	function custom_login_logo() {
+		?>
+		<style type="text/css">
+			body.login div#login h1 a {
+				background-image: url('https://oopscoder.com/wp-content/uploads/2025/04/login_logo.png');
+				background-size: contain;
+				width: 220px;
+				height: 100px;
+			}
+		</style>
+		<?php
+	}
+	add_action('login_head', 'custom_login_logo');
+
+	// Optional: Change login link URL and title
+	add_filter('login_headerurl', function() { return home_url(); });
+	add_filter('login_headertext', function() { return get_bloginfo('name'); });
+
 	// Added towards the end - for overriding footer notes
 	add_filter('generate_copyright', 'custom_generatepress_footer');
 
