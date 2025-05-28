4d01747f01442f428095e3a9ea52861986ac374a
srimantasahu <srimant.sahu@gmail.com>
2025-05-28 20:45:11 +0530

    fixed comma separator for categories links
    -- fixed blockquote font size issue

--- src/main/setup/php/post-meta.php
+++ src/main/setup/php/post-meta.php
@@ -1,7 +1,13 @@
 <?php
 
+// removed comma separator
+
+if ( 'categories' === $item ) {
+		$term_separator = apply_filters( 'generate_term_separator', _x( ' ', 'Used between list items, there is a space after the comma.', 'generatepress' ), 'categories' );
+		...
+}
+
 if ( 'tags' === $item ) {
-    // removed comma separator
-    $term_separator = apply_filters( 'generate_term_separator', _x( '', 'Used between list items, there is a space after the comma.', 'generatepress' ), 'tags' );
+    $term_separator = apply_filters( 'generate_term_separator', _x( ' ', 'Used between list items, there is a space after the comma.', 'generatepress' ), 'tags' );
     ...
 }
